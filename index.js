00fe39c64d7826cd8a318311b08113b8b3b9afc4
Hun <hunhoha@cmail.carleton.ca>
2020-02-12 16:33:55 -0500

    changed the greating

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ Bye: "Yo" });
 });
 
-// If it is all capital letter, 
-//it means that We should not change it 
+// If it is all capital letter,
+//it means that We should not change it
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
