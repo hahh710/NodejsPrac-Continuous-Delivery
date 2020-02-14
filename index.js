5f0b6cb6c7a3e3a2c88dc4a2ee8f39eec0291f61
Hun <hunhoha@cmail.carleton.ca>
2020-02-14 13:39:28 -0500

    Update

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 
-const app = express();
+const passport = require("passport"),
+  refresh = require("passport-oauth2-refresh");
 
-app.get("/", (req, res) => {
-  res.send({ Bye: "Yo", hi:"hi" });
-});
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const app = express();
+const keys = require("./config/keys.js");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken", accessToken);
+      console.log("RefreshToken", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+//Route for callback from google permission
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // If it is all capital letter,
 //it means that We should not change it
